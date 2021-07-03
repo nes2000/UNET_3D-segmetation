40841e6c062d2fd2c890ef87c31fa0a791b22b52
chakerouari <57746592+chakerouari@users.noreply.github.com>
2021-07-03 11:45:41 +0100

    rename predect image

--- process_data.py
+++ process_data.py
@@ -199,7 +199,7 @@ def main():
 
     for row in tqdm(df['img'].values):
 
-        img = np.array(Image.open(os.path.join(images_path,'croped_'+row)).convert('L'))
+        img = np.array(Image.open(os.path.join(images_path,row)).convert('L'))
 
         features = process_image(img,kernels)
 
